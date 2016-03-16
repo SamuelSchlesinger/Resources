bc78b48151712d90fb3bc4445f346e1fd50738d2
InstallScript <installscript@izbicki.me>
2016-03-16 18:50:15 -0400

    Hey

--- dexp.hs
+++ dexp.hs
@@ -5,15 +5,15 @@ data Exp = A Exp Exp -- Addition
          deriving (Show)
 
 d :: String -> Exp -> Exp --- d x a = da/dx
-clean :: Exp -> Exp       --- Cleans up expressions
-
-d x (A a b) = A (d x a) (d x b)             -- derivative is linear
-d x (M a b) = A (M (d x a) b) (M a (d x b)) -- product rule
+d x (A a b) = A (d x a) (d x b)             -- linear: d(a + b)/dx = da/dx + db/dx
+d x (M a b) = A (M (d x a) b) (M a (d x b)) -- product rule: d(a*b)/dx = a*db/dx + b*da/dx
 d x (S n) | (x == n)  = D 1.0               -- dx/dx = 1
           | otherwise = D 0.0               -- dy/dx = 0 assuming y independent of x
 d x (D _) = D 0.0                           -- for all r in R dr/dx = 0
 
 ---------------------------------------------------
+
+clean :: Exp -> Exp       --- cleans up expressions
 
 clean (A a (D 0.0)) = clean a               -- a + 0 = a 
 clean (A (D 0.0) b) = clean b               -- 0 + b = b
